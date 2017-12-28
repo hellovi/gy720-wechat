3aee247f2a918f7bfc017611b0b04c19141af04e
zhoumenglin <512648622@qq.com>
2017-12-28 12:03:38 +0800

    feat(Photographers):摄影师列表基本逻辑

--- src/views/Photographers.vue
+++ src/views/Photographers.vue
@@ -5,13 +5,14 @@
 
     <!-- 摄影师列表 -->
     <ul class="photographer-list">
-      <li>
-        <Photographer/>
+      <li
+       v-for="item in data"
+       :key="item.hash_user_id"
+      >
+        <Photographer :data="item"/>
       </li>
     </ul>
 
-    <!-- 底部导航 -->
-    <AppNavi />
   </div>
 </template>
 
@@ -19,13 +20,37 @@
 
 export default {
   name: 'Photographers',
+
+  data() {
+    return {
+      data: [],
+    }
+  },
+
+  methods: {
+    // 获取摄影师数据
+    getData(page) {
+      this.$http.get(`/wechatapi/author?page=${page}`)
+        .then(({ result: { authors } }) => {
+          this.data = [...this.data, ...authors.data]
+        })
+    },
+  },
+
+  created() {
+    this.getData(1)
+  },
 }
 </script>
 
-<style>
+<style lang="postcss">
 .photographer-list {
   padding: 0 20px;
   list-style: none;
   margin: 0;
+
+  & li:not(:last-child) {
+    margin-bottom: 20px;
+  }
 }
 </style>
