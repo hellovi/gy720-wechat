3e640885c37f2223458c82d714300ba08ad22d2a
chenliangshan <729755202@qq.com>
2017-12-29 18:08:27 +0800

    feat(mixins): 新增微信sdk分享处理

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line
 export { default as scroller } from './scroller'
+export { default as weixinsdk } from './weixinsdk'
---
+++ src/mixins/weixinsdk.js
@@ -0,0 +1,116 @@
+/*
+ * 微信SDK及注册分享功能
+ * @Author: chenliangshan
+ * @Date: 2017-12-29 16:51:16
+ * @Last Modified by: chenliangshan
+ * @Last Modified time: 2017-12-29 17:47:11
+ */
+
+import wx from 'weixin-js-sdk'
+
+export default {
+  data() {
+    return {
+      shareInfo: {},
+      weixinSDKConfig: {},
+    }
+  },
+
+  methods: {
+    getWxJsdk(url = window.location.href) {
+      return this.$http.get(`/wechatapi/jssdk?url=${encodeURIComponent(url)}`)
+        .then(({
+          result,
+        }) => {
+          wx.config(result.sdk)
+          this.weixinSDKConfig = { ...result,
+          }
+          return result
+        })
+    },
+
+    setWxReady(info = this.shareInfo) {
+      const {
+        seo,
+        shareLogo,
+      } = this.weixinSDKConfig
+      const config = {
+        title: document.title, // 分享标题
+        link: window.location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: shareLogo, // 分享图标
+        desc: `${seo.keywords} -- ${seo.description}`, // 分享描述
+        ...info,
+      }
+      wx.ready(() => {
+        // 分享到朋友圈
+        wx.onMenuShareTimeline({
+          success: () => {
+            // 用户确认分享后执行的回调函数
+          },
+          ...config,
+          title: config.desc,
+        })
+
+        // 分享给朋友
+        wx.onMenuShareAppMessage({
+          // type: '', // 分享类型,music、video或link，不填默认为link
+          // dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: () => {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: () => {
+            // 用户取消分享后执行的回调函数
+          },
+          ...config,
+        })
+
+        // 分享到QQ
+        wx.onMenuShareQQ({
+          success: () => {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: () => {
+            // 用户取消分享后执行的回调函数
+          },
+          ...config,
+        })
+
+        // 分享到腾讯微博
+        wx.onMenuShareWeibo({
+          success: () => {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: () => {
+            // 用户取消分享后执行的回调函数
+          },
+          ...config,
+        })
+
+        // 分享到QQ空间
+        wx.onMenuShareQZone({
+          success: () => {
+            // 用户确认分享后执行的回调函数
+          },
+          cancel: () => {
+            // 用户取消分享后执行的回调函数
+          },
+          ...config,
+        })
+      })
+    },
+  },
+
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.getWxJsdk()
+        .then(() => {
+          vm.setWxReady()
+        })
+    })
+  },
+
+  beforeRouteUpdate(to, from, next) {
+    next()
+    this.setWxReady()
+  },
+}
