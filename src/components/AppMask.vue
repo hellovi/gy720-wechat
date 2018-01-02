2ba3c014a867a199f69368abb7fd77c741dbd9af
zhoumenglin <512648622@qq.com>
2018-01-02 17:57:56 +0800

    feat(AppMask):遮罩层组件

---
+++ src/components/AppMask.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="mask"
+    v-if="!scene.ok"
+  >
+
+    <!-- 进度条 -->
+    <Progress
+      v-if="scene.percent > 0"
+      :percent="scene.percent"
+    />
+
+    <!-- 提示信息 -->
+    <span v-else >{{scene.message}}</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    scene: {
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style lang="postcss">
+@import 'vars.css';
+
+.mask {
+  position: absolute;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color:rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & > span {
+    font-size: 30px;
+    color: #fff;
+  }
+}
+
+</style>
