29d987323faabf48fb1235a9e94ed511cc0a056a
zhoumenglin <512648622@qq.com>
2018-01-04 16:55:59 +0800

    feat(ImgMask):透明度修改

--- src/components/ImgMask.vue
+++ src/components/ImgMask.vue
@@ -34,7 +34,7 @@ export default {
   bottom: 0;
   left: 0;
   right: 0;
-  background-color:rgba(0, 0, 0, 0.6);
+  background-color:rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
